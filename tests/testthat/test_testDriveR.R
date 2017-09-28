2e853a46279ef8fe5e2f3f6bba5e283b292ab8fe
Christopher Prener <chris.prener@slu.edu>
2017-09-28 09:21:03 -0500

    fix unit test data frame refs

--- tests/testthat/test_testDriveR.R
+++ tests/testthat/test_testDriveR.R
@@ -1,8 +1,8 @@
 context("Data check")
 
 test_that("data objects are unchanged", {
-  expect_equal_to_reference(stlAsthma, "auto17.Rdata")
-  expect_equal_to_reference(stlInsurance, "gss16.RData")
-  expect_equal_to_reference(stlLead, "gss16_Miss.RData")
-  expect_equal_to_reference(stlMurders, "kerrich.RData")
+  expect_equal_to_reference(aut17, "auto17.Rdata")
+  expect_equal_to_reference(gss16, "gss16.RData")
+  expect_equal_to_reference(gss16_Miss, "gss16_Miss.RData")
+  expect_equal_to_reference(kerrich, "kerrich.RData")
 })
