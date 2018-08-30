71005724433f9a559c00293bc95c2bf54d58ada6
Christopher Prener <chris.prener@slu.edu>
2018-08-30 11:02:58 -0500

    update unit tests

--- tests/testthat/test_testDriveR.R
+++ tests/testthat/test_testDriveR.R
@@ -3,7 +3,5 @@ context("Data check")
 test_that("data objects are unchanged", {
   expect_equal_to_reference(auto17, "auto17.Rdata")
   expect_equal_to_reference(childMortality, "childMortality.RData")
-  expect_equal_to_reference(gss16, "gss16.RData")
-  expect_equal_to_reference(gss16_Miss, "gss16_Miss.RData")
-  expect_equal_to_reference(kerrich, "kerrich.RData")
+  expect_equal_to_reference(kerrich, "kerrich.rda")
 })
